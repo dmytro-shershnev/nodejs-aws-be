a25a0f06290aeedc6314cefbd28c19a729503a3a
Dmytro Shershnev <Dmytro.Shershnev@goto.com>
2022-07-20 23:12:01 +0300

    Task-3: Update products list with new IDs

--- product-service/src/functions/getProductsById/handler.ts
+++ product-service/src/functions/getProductsById/handler.ts
@@ -2,12 +2,12 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 
-import PRODUCTS from '../../mock/products.json';
+import products from '../../mock/products.json';
 
 const getProductsById = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const id = event.pathParameters.id;
 
-  const product = PRODUCTS.filter(item => item.id === id);
+  const product = products.filter(item => item.id === id);
 
   if (!product || !product.length) {
     return formatJSONResponse(404,{
